10e183a589518f4c0b46afee46e07f52a94257ae
jinnosukeKato <jinnosukeKato@gmail.com>
2024-07-14 16:26:43 +0900

    アセンブリ言語とアセンブラを実装

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,18 @@
 package io.github.jinnosukeKato
 
+import io.github.jinnosukeKato.assembler.Compiler
+import io.github.jinnosukeKato.assembler.Parser
 import io.github.jinnosukeKato.cpu.TD4
 
 fun main() {
+    val parser = Parser("./src/main/resources/test3.tdasm")
+    val tokenList = parser.parse()
+    val compiler = Compiler(tokenList)
+    val bin = compiler.compile()
+    bin.forEach{
+        println(it.toString(2).padStart(8, '0'))
+    }
+
     val cpu = TD4("./src/main/resources/test2.bin", 0b1010)
     for (i in 0..100) {
         val data = cpu.fetch()
---
+++ src/main/kotlin/assembler/Parser.kt
@@ -0,0 +1,76 @@
+package io.github.jinnosukeKato.assembler
+
+import java.io.File
+
+class Parser(sourcePath: String) {
+
+    private val source: List<String> = buildList {
+        File(sourcePath)
+            .bufferedReader()
+            .readLines()
+            .forEach { line ->
+                line.split(' ')
+                    .forEach {
+                        add(it)
+                    }
+        }
+    }
+
+    fun parse(): List<Token> {
+        var pos = 0
+
+        return buildList {
+            while (pos < source.size) {
+                val op = source[pos]
+                when {
+                    op == "MOV" -> {
+                        pos += 1
+                        val left = source[pos]
+
+                        pos += 1
+                        val right = source[pos]
+
+                        if (left == "A" && right == "B")
+                            add(Token.MovAB)
+                        else if(left == "B" && right == "A")
+                            add(Token.MovBA)
+                        else
+                            add(Token.Mov(Register.getByString(left), right.toInt(2)))
+                    }
+                    op == "ADD" -> {
+                        pos += 1
+                        val left = source[pos]
+
+                        pos += 1
+                        val right = source[pos]
+
+                        add(Token.Add(Register.getByString(left), right.toInt(2)))
+                    }
+                    op == "JMP" -> {
+                        pos += 1
+                        add(Token.Jmp(source[pos].toInt(2)))
+                    }
+                    op == "JNC" -> {
+                        pos += 1
+                        add(Token.Jnc(source[pos].toInt(2)))
+                    }
+                    op == "IN" -> {
+                        pos += 1
+                        add(Token.In(Register.getByString(source[pos])))
+                    }
+                    op == "OUT" -> {
+                        pos += 1
+                        if (source[pos] == "B")
+                            add(Token.OutB)
+                        else
+                            add(Token.OutIm(source[pos].toInt(2)))
+                    }
+                    op.isBlank() -> break
+                    else -> throw RuntimeException("Illegal Opcode $op")
+                }
+
+                pos += 1
+            }
+        }
+    }
+}
